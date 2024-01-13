a9ddf3c87ae8d57f2e5d6de49a2ea85268339e9d
filipe.dias <fedispato@gmail.com>
2024-01-13 16:59:08 -0300

    feat: Configurando arquivo de propriedades

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
 	kotlin("plugin.spring") version "1.9.21"
 	kotlin("plugin.jpa") version "1.9.21"
 	kotlin("plugin.allopen") version "1.9.21"
+	kotlin("kapt") version "1.9.21"
 }
 
 group = "com.example"
@@ -34,6 +35,7 @@ dependencies {
 	testImplementation("org.junit.jupiter:junit-jupiter-api")
 	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 	testImplementation("com.ninja-squad:springmockk:4.0.2")
+	kapt("org.springframework.boot:spring-boot-configuration-processor")
 }
 
 tasks.withType<KotlinCompile> {
